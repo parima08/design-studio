56986fd83ebcf20f2b1543dfb83e1caecad5a560
Parima <parima@openspace.ai>
2019-10-11 17:10:55 -0700

    Updates

---
+++ precache-manifest.5ee1dd762cb1cf93f8ec30b688677def.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0c284175a35a8a1f59ed1aed110fd20",
+    "url": "/design-studio/index.html"
+  },
+  {
+    "revision": "55c0988b2312f2d83a3d",
+    "url": "/design-studio/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "191044ec1d02709f9589",
+    "url": "/design-studio/static/js/2.890993e7.chunk.js"
+  },
+  {
+    "revision": "55c0988b2312f2d83a3d",
+    "url": "/design-studio/static/js/main.0a2fed48.chunk.js"
+  },
+  {
+    "revision": "9aa1c9472b123f40e67a",
+    "url": "/design-studio/static/js/runtime-main.a41b4cf2.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/design-studio/static/media/logo.25bf045c.svg"
+  }
+]);
